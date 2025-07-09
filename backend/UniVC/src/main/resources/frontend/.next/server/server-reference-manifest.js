19a36bfa62ccde6e349da2ea61c4faae54386348
amaydixit11 <amaydixit11@gmail.com>
2025-07-09 15:19:05 +0530

    frontend fix

--- backend/UniVC/src/main/resources/frontend/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"ZwY8NfALoOVmRnLivdIN9CVbk7mR4zpxNRE3IQ6Hl+c=\"\n}"
--- backend/UniVC/src/main/resources/frontend/.next/static/chunks/src_app_page_tsx_cf4a38cf._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_cea6d099._.js",
-  "static/chunks/node_modules_900e19a1._.js"
-],
-    source: "dynamic"
-});
